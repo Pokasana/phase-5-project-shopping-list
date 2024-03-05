e9f6bcd6cc3df5fbba66f0ba926220093d7a2121
Sanae <sanae.s.soma@gmail.com>
2024-03-05 00:37:10 -1000

    add User model and migration for the users table

---
+++ server/migrations/versions/b463508ba7c3_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: b463508ba7c3
+Revises: 
+Create Date: 2024-03-04 20:33:51.951434
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b463508ba7c3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- server/models.py
+++ server/models.py
@@ -4,3 +4,11 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+
+    def  __repr__(self):
+        return f'User: {self.name}'
