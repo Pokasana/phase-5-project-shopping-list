f6fe55b7f172a9e3e6b7e9318160014a6df0eac8
Sanae <sanae.s.soma@gmail.com>
2024-03-11 22:44:23 -1000

    replace handleChange and handleSubmit in AddShopForm.js to Formik

--- client/src/components/AddShopForm.js
+++ client/src/components/AddShopForm.js
@@ -1,35 +1,38 @@
 import React, { useState } from "react";
+import { useFormik } from "formik";
 
 function AddShopForm({ onAddShop }) {
-  const [formData, setFormData]  = useState("")
 
-  function handleInput(e) {
-    setFormData(e.target.value);
-  };
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    fetch("http://127.0.0.1:5555/shops", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(formData)
-    })
-    .then(r => r.json())
-    .then(newShop => {
-      onAddShop(newShop);
-    })
-
-    setFormData("")
-
-  }
+  const formik = useFormik({
+    initialValues: {
+      shop: "",
+    },
+    onSubmit: (values, { resetForm }) => {
+      fetch("http://127.0.0.1:5555/shops", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(values.shop)
+      })
+      .then(r => r.json())
+      .then(newShop => {
+        onAddShop(newShop);
+      })
+      resetForm()
+    }
+  })
 
   return (
-    <form className="add_shop" onSubmit={handleSubmit}>
+    <form className="add_shop" onSubmit={formik.handleSubmit}>
       <h3>Add a new shop</h3>
-      <input type="text" value={formData} onChange={handleInput}/>
+      <input
+        id="shop"
+        name="shop"
+        type="shop"
+        onChange={formik.handleChange}
+        value={formik.values.shop}
+      />
       <button type="submit">Add</button>
     </form>
   )
