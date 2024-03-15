ec3344eeb8465bf913ee157752eae26d101342c9
Sanae <sanae.s.soma@gmail.com>
2024-03-14 19:58:25 -1000

    delete unused codes and imports from Shops.js

--- client/src/components/Shops.js
+++ client/src/components/Shops.js
@@ -1,37 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import AddShopForm from "./AddShopForm"
 
 function Shops({ shopsList, onAddShop, clickHandler, isLoaded }) {
-	// const [shops, setShops] = useState([])
-  // const [isLoaded, setIsLoaded] = useState(false)
-
-	// useEffect(() => {
-	// fetch('http://127.0.0.1:5555/shops')
-	// .then(r => r.json())
-	// .then(data => {
-	// 	setShops(data);
-	// 	setIsLoaded(true);
-	// })
-	// }, [])
 
   if (!isLoaded) return <h3>Loading...</h3>
-
-  // function onAddShop(newShop) {
-  //   setShops([...shops, newShop])
-  // };
-
-	// function clickHandler(id) {
-
-	// 	fetch(`http://127.0.0.1:5555/shops/${id}`, {
-	// 		method: "DELETE",
-	// 	})
-	// 	.then(r => r.json())
-	// 	.then(() => {
-	// 		setShops(shops => {
-	// 			return shops.filter(shop => shop.id !== id)
-	// 		})
-	// 	})
-	// };
 
 	return (
 		<div className="shops">
