a7bb9875e580d4c562f0b23589e09ef51c986af0
Sanae <sanae.s.soma@gmail.com>
2024-04-30 16:17:14 -1000

    add async thunk fetchItems in itemsSlice

--- client/src/features/items/itemsSlice.js
+++ client/src/features/items/itemsSlice.js
@@ -1,15 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
 const initialState = []
 
+export const fetchItems =  createAsyncThunk('items/fetchItems', async () => {
+  const response = await fetch('http://127.0.0.1:5555/items')
+	const data = await response.json()
+	return data
+})
+
 const itemsSlice = createSlice({
-    name: 'items',
-    initialState,
-    reducers: {
-        setItems: (state, action) => {
-            state.push(action.payload)
-        }
-    }
+	name: 'items',
+	initialState,
+	reducers: {},
+	extraReducers(builder) {
+		builder.addCase(fetchItems.fulfilled, (state, action) => {
+			return action.payload
+		})
+	}
 })
 
 export default itemsSlice.reducer
+
+export const selectAllItems = state => state.items
+export const selectItemById = (state, itemId) => state.items.find(item => item.id === itemId)
